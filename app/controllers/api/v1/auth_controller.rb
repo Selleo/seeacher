8eec62a73c50d00b43a09305f47eae36f36d7040
dpienczykowski <dpienczykowski@gmail.com>
2018-05-15 22:44:49 +0200

    fix auth

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,9 +1,5 @@
 class Api::V1::AuthController < Api::V1::BaseApiController
   def is_signed_in?
-    if user_signed_in?
-      render json: { 'signed_in' => true, 'user' => current_user }.to_json
-    else
-      render json: {}, status: 403
-    end
+    render json: { 'signed_in' => user_signed_in? }.to_json
   end
 end
