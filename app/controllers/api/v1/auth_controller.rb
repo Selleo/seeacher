9b071539f898c9c12c4fa8441152139a488b55f7
dpienczykowski <dpienczykowski@gmail.com>
2018-06-18 10:42:10 +0200

    display user if signed

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::AuthController < Api::V1::BaseApiController
   def is_signed_in?
-    render json: { 'signed_in' => user_signed_in? }.to_json
+    render json: { 'signed_in' => user_signed_in?, 'user' => current_user }.to_json
   end
 end
